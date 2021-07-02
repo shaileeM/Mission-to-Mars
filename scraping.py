b7cd83bdbb92764182ba717e5f69019afd803d62
Shailee <shailee.madhani@gmail.com>
2021-07-01 21:10:12 -0700

    getting there

--- scraping.py
+++ scraping.py
@@ -1,13 +1,29 @@
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-#from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import numpy
+import datetime as dt
 
 
-executable_path = {'executable_path':'C:\\Users\shail\.wdm\drivers\chromedriver\win32\91.0.4472.101\chromedriver.exe'}
-#browser = Browser('chrome', **executable_path, headless=False)
-browser = Browser('chrome',**executable_path,headless=False)
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    
+    news_title, news_paragraph = mars_news(browser)
+    # Run all scraping functions and store results in dictionary
+    data = {
+      "news_title": news_title,
+      "news_paragraph": news_paragraph,
+      "featured_image": featured_image(browser),
+      "facts": mars_facts(),
+      "last_modified": dt.datetime.now()
+    }
+    
+#executable_path = {'executable_path':'C:\\Users\shail\.wdm\drivers\chromedriver\win32\91.0.4472.101\chromedriver.exe'}
+
 
 def mars_news(browser):
 
